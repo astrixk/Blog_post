2375d10d06895777f5faaa6d787dbedf600c024d
Sampath Guruprasad <sampathguruprasad@gmail.com>
2024-12-05 23:07:18 +0530

    Adding mongodb

--- app.js
+++ app.js
@@ -15,3 +15,13 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const uri = process.env.MONGO_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.error("Connection failed", err));
